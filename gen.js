96f1bd5e3900aba3f5b02b398effa22d264c7361
Elana Kopelevich <ekopelevich@gmail.com>
2016-11-15 17:15:18 -0700

    generator example

---
+++ gen.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function* idMaker(){
+  var index = 0;
+  while(true)
+    yield index++
+}
+
+var gen = idMaker()
+
+console.log(gen.next().value)
+console.log(gen.next().value) 
+console.log(gen.next().value)
+
